a6940577f7e86014fc7e9e4dc85f85f47f568b13
StarPool <seitetsu@bembegi.com>
2024-11-30 00:21:03 +0900

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -9,8 +9,8 @@ disk_size_gb=$(parted /dev/sda --script print | awk '/^Disk \/dev\/sda:/ {print 
 disk_size_mb=$((disk_size_gb * 1024))
 
 #Calculate partition size (25% of total size)
-#part_size_mb=$((disk_size_mb / 4))
-part_size_mb=$((200 * 1024))
+part_size_mb=$((disk_size_mb / 4))
+#part_size_mb=$((200 * 1024))
 
 #Create GPT partition table
 parted /dev/sda --script -- mklabel gpt
